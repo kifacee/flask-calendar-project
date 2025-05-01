b2a860c6614d78f69ba7580b11df765e9839ccda
Kian <kifacee@gmail.com>
2025-05-01 12:01:24 -0700

    basic setup done

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from app import routes      #from 'app' folder import 'routes.py'
+from app.config import Config
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+print("SECRET KEY IS: ", app.config["SECRET_KEY"])
+# alternative if not using a Config class
+# app.config.update({'SECRET_KEY': os.environ.get('SECRET_KEY')}) #update the config dictionary with the secret key env variable
+
+app.register_blueprint(routes.bp)       #register the 'main' blueprint
